cf3883bee54c5f95919a6ce48744b2119825b9e5
Ali Sills <alisill@mail.regent.edu>
2021-09-06 14:55:36 -0400

    finished mini project

--- app.js
+++ app.js
@@ -2,4 +2,8 @@
 const kelvin = 293;
 // Celsius conversion
 let celsius = kelvin - 273;
-console.log(celsius)
+// Fahrenheit equation
+let fahrenheit = celsius * (9/5) + 32;
+// Rounds down
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degress Fahrenheit.`)
